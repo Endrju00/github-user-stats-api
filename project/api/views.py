414fae7a2b82bbefe15b2fe85d1b667265f9e71d
AM KK <75850182+Endrju00@users.noreply.github.com>
2022-01-04 10:19:39 +0100

    Add API overview

--- project/api/views.py
+++ project/api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 # Create your views here.
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'List of repositories': '/user-repositories/',
+        'Sum of stars': '/stars-counter/',
+        'The most popular programming languages': '/programming-languages/'
+    }
+    return Response(api_urls)
