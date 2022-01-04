15a947d4f4d74fc723b4420df9cf117459d33a9b
AM KK <75850182+Endrju00@users.noreply.github.com>
2022-01-04 14:36:01 +0100

    Add userProgrammingLanguages view

--- project/api/urls.py
+++ project/api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.apiOverview, name="api-overview"),
     path('<str:username>/repositories/', views.userRepositories, name='user-repositories'),
     path('<str:username>/sum-of-stars/', views.userSumOfStars, name='user-stars'),
+    path('<str:username>/programming-languages/', views.userProgrammingLanguages, name='user-programming-languages'),
 ]
--- project/api/views.py
+++ project/api/views.py
@@ -45,3 +45,22 @@ def userSumOfStars(request, username):
         star_counter += repository["stargazers_count"]
     
     return Response({"sum_of_stars": star_counter})
+
+
+@api_view(['GET'])
+def userProgrammingLanguages(request, username):
+    github = GitHubAPI()
+    repositories = github.getUserRepositories(username)
+    languages = {}
+
+    for repository in repositories:
+        if not repository["language"]:
+            continue
+        elif languages.get(repository["language"]):
+            languages[repository["language"]] += repository["size"]
+        else:
+            languages[repository["language"]] = repository["size"]
+    
+    languages = dict(sorted(languages.items(), key=lambda k: (k[1], k[0]), reverse=True))  # Sort descending by size
+
+    return Response(languages)
