0fdb35634924aa4948673393bcc616f931453191
AM KK <75850182+Endrju00@users.noreply.github.com>
2022-01-04 13:28:41 +0100

    Add userRepositories view

--- project/api/urls.py
+++ project/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.apiOverview, name="api-overview"),
+    path('<str:username>/repositories/', views.userRepositories, name='user-repositories'),
 ]
--- project/api/views.py
+++ project/api/views.py
@@ -4,6 +4,8 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from . github import GitHubAPI
+
 
 # Create your views here.
 @api_view(['GET'])
@@ -14,3 +16,18 @@ def apiOverview(request):
         'The most popular programming languages': '/<username>/programming-languages/'
     }
     return Response(api_urls)
+
+
+@api_view(['GET'])
+def userRepositories(request, username):
+    github = GitHubAPI()
+    repositories = github.getUserRepositories(username)
+    data = []
+    
+    for repository in repositories:
+        d = {}
+        d["name"] = repository["name"]
+        d["stars"] = repository["stargazers_count"]
+        data.append(d)
+
+    return Response(data)
