f980c92bbc73cef9412812df5be23df2e8941e17
AM KK <75850182+Endrju00@users.noreply.github.com>
2022-01-05 16:41:13 +0100

    Update API Overview

--- project/api/views.py
+++ project/api/views.py
@@ -23,6 +23,7 @@ def apiOverview(request) -> Response:
     """
 
     api_urls = {
+        'Format': 'Add /?format=json/ to get json or leave blank for user-friendly version.',
         'List of repositories': '/<username>/repositories/',
         'Sum of stars': '/<username>/sum-of-stars/',
         'The most popular programming languages': '/<username>/programming-languages/'
