d4cb81a3c82ee6aa57428305d6365463f4c4106e
AM KK <75850182+Endrju00@users.noreply.github.com>
2022-01-05 14:40:12 +0100

    Add handling errors from GitHub API

--- project/api/github.py
+++ project/api/github.py
@@ -21,11 +21,42 @@ class GitHubAPI():
 
         page = 1
         r = requests.get(f'https://api.github.com/users/{username}/repos?per_page=100&page={page}')    
-        data = r.json()
 
-        while r.json():  # Check if there are more pages
-            page += 1
-            r = requests.get(f'https://api.github.com/users/{username}/repos?per_page=100&page={page}')
-            data += r.json()
+        if r.status_code == 200:
+            data = r.json()
+
+            while r.json():  # Check if there are more pages
+                page += 1
+                r = requests.get(f'https://api.github.com/users/{username}/repos?per_page=100&page={page}')
+                
+                if r.status_code == 200:
+                    data += r.json()
+                else:
+                    error = self.errorHandler(r.status_code)
+                    return error
+        
+        else:
+            error = self.errorHandler(r.status_code)
+            return error
         
         return data
+
+    def errorHandler(self, status_code: int) -> Dict: 
+        """
+        Returns information about the error resulting from the status code.
+
+                Parameters:
+                        status_code: request status code
+                
+                Returns: Dict with information about error and error code
+        """
+        
+        error = {
+            "info": "Something went wrong with GitHub API",
+            "error_code": status_code
+        }
+
+        if status_code == 404:
+            error["info"] = "User not found"
+
+        return error
--- project/api/views.py
+++ project/api/views.py
@@ -40,22 +40,26 @@ def userRepositories(request, username: AnyStr) -> Response:
                     username: GitHub username for which the data will obtained
             
             Returns:
-                    Response(data): Django Rest Framework response - name and number of stars of user's every public repository
+                    Response(data): Django Rest Framework response - name and number of stars of user's every public repository or info with the error code
     """
 
     github = GitHubAPI()
     repositories = github.getUserRepositories(username)
-    data = []
     
-    for repository in repositories:
-        extracted = {
-            "name": repository["name"],
-            "stars": repository["stargazers_count"]
-        }
+    if repositories.get("error_code"):
+        return Response(repositories)
+    else:
+        data = []
+        
+        for repository in repositories:
+            extracted = {
+                "name": repository["name"],
+                "stars": repository["stargazers_count"]
+            }
 
-        data.append(extracted)
+            data.append(extracted)
 
-    return Response(data)
+        return Response(data)
 
 
 @api_view(['GET'])
@@ -68,17 +72,21 @@ def userSumOfStars(request, username: AnyStr) -> Response:
                     username: GitHub username for which the data will obtained
             
             Returns:
-                    Response(data): Django Rest Framework response - user's sum of stars 
+                    Response(data): Django Rest Framework response - user's sum of stars or info with the error code
     """
 
     github = GitHubAPI()
     repositories = github.getUserRepositories(username)
-    star_counter = 0
 
-    for repository in repositories:
-        star_counter += repository["stargazers_count"]
-    
-    return Response({"sum_of_stars": star_counter})
+    if repositories.get("error_code"):
+        return Response(repositories)
+    else: 
+        star_counter = 0
+
+        for repository in repositories:
+            star_counter += repository["stargazers_count"]
+        
+        return Response({"sum_of_stars": star_counter})
 
 
 @api_view(['GET'])
@@ -91,21 +99,25 @@ def userProgrammingLanguages(request, username: AnyStr) -> Response:
                     username: GitHub username for which the data will obtained
             
             Returns:
-                    Response(data): Django Rest Framework response - user's favourite programming languages
+                    Response(data): Django Rest Framework response - user's favourite programming languages or info with the error code
     """
 
     github = GitHubAPI()
     repositories = github.getUserRepositories(username)
-    languages = {}
 
-    for repository in repositories:
-        if not repository["language"]:
-            continue
-        elif languages.get(repository["language"]):
-            languages[repository["language"]] += repository["size"]
-        else:
-            languages[repository["language"]] = repository["size"]
-    
-    languages = dict(sorted(languages.items(), key=lambda k: (k[1], k[0]), reverse=True))  # Sort descending by size
+    if repositories.get("error_code"):
+        return Response(repositories)
+    else:
+        languages = {}
 
-    return Response(languages)
+        for repository in repositories:
+            if not repository["language"]:
+                continue
+            elif languages.get(repository["language"]):
+                languages[repository["language"]] += repository["size"]
+            else:
+                languages[repository["language"]] = repository["size"]
+        
+        languages = dict(sorted(languages.items(), key=lambda k: (k[1], k[0]), reverse=True))  # Sort descending by size
+
+        return Response(languages)
